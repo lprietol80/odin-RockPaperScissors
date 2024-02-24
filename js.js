5442f6c4ce598d83388b7eef7db1d7f0ba057489
lprietol80 <eskriba23@outlook.com>
2024-02-24 15:02:16 -0500

    reset score function, finish the project

--- js.js
+++ js.js
@@ -1,20 +1,26 @@
 // Game options//
+
 let gameOptions = ["rock","paper","scissors"];
 
 
 const rock_btn=document.getElementById("rock");
 const paper_btn=document.getElementById("paper");
 const scissors_btn=document.getElementById("scissors");
+
+
 rock_btn.addEventListener("click",()=>{
   const computerSelection = getComputerChoice();
   const playerSelection="rock";
-  playRound(playerSelection,computerSelection)
+  playRound(playerSelection,computerSelection);
+  fightWinner(playerScore,computerScore,tieScore);
+  
 }
 );
 paper_btn.addEventListener("click",()=>{
   const computerSelection = getComputerChoice();
   const playerSelection="paper";
-  playRound(playerSelection,computerSelection)
+  playRound(playerSelection,computerSelection);
+  fightWinner(playerScore,computerScore,tieScore);
 }
 );
 
@@ -22,7 +28,8 @@ scissors_btn.addEventListener("click",
 ()=>{
   const computerSelection = getComputerChoice();
   const playerSelection="scissors";
-  playRound(playerSelection,computerSelection)
+  playRound(playerSelection,computerSelection);
+  fightWinner(playerScore,computerScore,tieScore);
 }
 );
 
@@ -35,38 +42,81 @@ function getComputerChoice (){
   return computerSelection
 }
 
+let playerScore=0;
+let computerScore=0;
+let tieScore=0;
 function checkWinner (player,computer){
   if (player==computer){
+    tieScore++
     return "tie"
   }else if (player === "rock" && computer==="scissors" ||
   player === "paper" && computer==="rock" || 
   player === "scissors" && computer==="paper"){
+    playerScore++
     return "player"
   }else{
+    computerScore++
     return "computer"
   }
 }  
 
+// fight winner
+function fightWinner (pScore,cScore,tScore){
+  if(pScore===5){
+    const FightW=document.querySelector(".winner").innerHTML=`<h1>you WIN, great job beating the computer</h1><button onclick="resetOutcome()">RESET GAME</button>`;
+    resetScore();
+
+
+  }if(cScore===5){
+    const FightW=document.querySelector(".winner").innerHTML=`<h1>you couldn't beat computer, computer WINS. </h1><button onclick="resetOutcome()">RESET GAME</button>`;
+    resetScore();
+  
+  }if(tScore===5){
+    const FightW=document.querySelector(".winner").innerHTML=`<h1>It was a hard fight, NO WINNER this time </h1><button onclick="resetOutcome()">RESET GAME</button>`;
+    resetScore();
+
+  }
+}
+
+
+
 //play round 
+
+
+
 function playRound(player, computer) {
   let result = checkWinner (player,computer);
   if (result==="tie"){
     const displayInfo = document.querySelector(".display").innerHTML= `
     <h2>This round:</h2><h3>
     Player chose: <img src="img/${player}.png" alt="scissors">, Computer chose:
-    <img src="img/${computer}.png" alt="scissors">, It is a TIE</h3>`;         
+    <img src="img/${computer}.png" alt="scissors">, It is a TIE</h3>`;
+
   }else if(result==="player"){
     const displayInfo = document.querySelector(".display").innerHTML= `<h2>This round:</h2> 
     <h3>Player chose: <img src="img/${player}.png" alt="scissors">, Computer chose: 
-    <img src="img/${computer}.png" alt="scissors">, <img src="img/${player}.png" alt="scissors">beats <img src="img/${computer}.png" alt="scissors">, Player WINS!!</h3>`
+    <img src="img/${computer}.png" alt="scissors">, <img src="img/${player}.png" alt="scissors">beats <img src="img/${computer}.png" alt="scissors">, Player WINS!!</h3>`;
+
   }else {
     const displayInfo = document.querySelector(".display").innerHTML= `<h2>This round:</h2>
     <h3>
     Player chose: <img src="img/${player}.png" alt="scissors">, Computer chose: <img src="img/${computer}.png" alt="scissors">, <img src="img/${computer}.png" alt="scissors"> beats <img src="img/${player}.png" alt="scissors">, computer WINS!!
-    </h3>`
+    </h3>`;
   }  
   
 }
 
+function resetScore() {
+  playerScore=0;
+  computerScore=0;
+  tieScore=0;
+  };
 
+
+
+function resetOutcome(){  
+    const FightW=document.querySelector(".winner").innerHTML=``;
+  }
   
+
+
