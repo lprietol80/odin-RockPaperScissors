40a1482533d86b872aceb0551bc4741611e41666
lprietol80 <eskriba23@outlook.com>
2024-02-24 10:19:06 -0500

    CSS improved,  playRound function was created

--- js.js
+++ js.js
@@ -1,99 +1,72 @@
-
-    
 // Game options//
-
 let gameOptions = ["rock","paper","scissors"];
 
-const allBtns=document.querySelectorAll(".btn-options");
 
-function  startGame (){  
-  allBtns.forEach(btn => {
-    btn.addEventListener("click",()=>{
-      if(btn.id ){
-        playRound(btn.id)
-        
-      }      
-    })     
-  })
-};
-startGame();
+const rock_btn=document.getElementById("rock");
+const paper_btn=document.getElementById("paper");
+const scissors_btn=document.getElementById("scissors");
+rock_btn.addEventListener("click",()=>{
+  const computerSelection = getComputerChoice();
+  const playerSelection="rock";
+  playRound(playerSelection,computerSelection)
+}
+);
+paper_btn.addEventListener("click",()=>{
+  const computerSelection = getComputerChoice();
+  const playerSelection="paper";
+  playRound(playerSelection,computerSelection)
+}
+);
+
+scissors_btn.addEventListener("click",
+()=>{
+  const computerSelection = getComputerChoice();
+  const playerSelection="scissors";
+  playRound(playerSelection,computerSelection)
+}
+);
+
+
 
 // Computer's choose an option //
+
 function getComputerChoice (){
-  let computerChoice = gameOptions[Math.floor(Math.random()*gameOptions.length)];
-  return computerChoice
+  let computerSelection = gameOptions[Math.floor(Math.random()*gameOptions.length)];
+  return computerSelection
 }
 
-
-// Check who wins//
-
-function checkWinner (choice1,choice2){  
- if (choice1==choice2){
-   return "tie"
- }else if (choice1 === "rock" && choice2==="scissors" ||
- choice1 === "paper" && choice2==="rock" || 
- choice1 === "scissor" && choice2==="paper"){
- return "player"
- }else{
-   return "computer"
- }
-}
-
-const winners =[];
-const winner = checkWinner ();
-winners.push(winner);
-function checkWins(){
-  const playerWinCount =winners.filter((item)=>item=="player").length ;
-  const compuWinCount= winners.filter((item)=>item=="computer").length;
-  return Math.max(playerWinCount ,compuWinCount)
-}
-
-
-
-//Play one round
-function playRound(playerSelection) {
-  const totalWins = checkWins();
-  if (totalWins>=5){
-    return "Game Over"
-  }
-  
-  const computerSelection = getComputerChoice();  
-  let result = checkWinner (playerSelection,computerSelection);
-  if (result==="tie"){
-    return document.querySelector(".result").innerHTML= `Player chose: ${playerSelection}, Computer chose: ${computerSelection}, It is a TIE`;    
-  }else if(result==="player"){
-    return document.querySelector(".result").innerHTML=`Player chose: ${playerSelection}, Computer chose: ${computerSelection}, ${playerSelection} beats ${computerSelection}, Player WINS!!`
-  }else {
-    return document.querySelector(".result").innerHTML=`Computer chose: ${computerSelection}, Player chose: ${playerSelection}, ${computerSelection} beats ${playerSelection}, Computer WINS!!`
-  }    
-}
- 
-//play the game 
-
-
-/*
-
-function game(playerSelection){
-  let playerScore = 0;
-  let computerScore=0;
-  const computerSelection = getComputerChoice(); 
-
-  for(let i=0;i<5;i++){
-    console.log(i)
-    if(checkWinner (playerSelection,computerSelection)==="player"){
-      playerScore++;
-    }else if(checkWinner (playerSelection,computerSelection)==="computer"){
-      computerScore++;    
-    }
-  }
-  if(playerScore>computerScore){
-    document.querySelector(".result").innerHTML= `Player's score:${playerScore}, Computer's score: ${computerScore},PLAYER IS THE WINNER` 
-  }else if(computerScore>playerScore){
-    document.querySelector(".result").innerHTML=`Computer's score: ${computerScore},Player's score: ${playerScore}, COMPUTER IS THE WINNER`;
+function checkWinner (player,computer){
+  if (player==computer){
+    return "tie"
+  }else if (player === "rock" && computer==="scissors" ||
+  player === "paper" && computer==="rock" || 
+  player === "scissors" && computer==="paper"){
+    return "player"
   }else{
-    document.querySelector(".result").innerHTML=`Computer's score: ${computerScore},Player's score: ${playerScore},  IT IS A TIE`
+    return "computer"
   }
-}
+}  
+
+//play round 
+function playRound(player, computer) {
+  let result = checkWinner (player,computer);
+  if (result==="tie"){
+    const displayInfo = document.querySelector(".display").innerHTML= `
+    <h2>This round:</h2><h3>
+    Player chose: <img src="img/${player}.png" alt="scissors">, Computer chose:
+    <img src="img/${computer}.png" alt="scissors">, It is a TIE</h3>`;         
+  }else if(result==="player"){
+    const displayInfo = document.querySelector(".display").innerHTML= `<h2>This round:</h2> 
+    <h3>Player chose: <img src="img/${player}.png" alt="scissors">, Computer chose: 
+    <img src="img/${computer}.png" alt="scissors">, <img src="img/${player}.png" alt="scissors">beats <img src="img/${computer}.png" alt="scissors">, Player WINS!!</h3>`
+  }else {
+    const displayInfo = document.querySelector(".display").innerHTML= `<h2>This round:</h2>
+    <h3>
+    Player chose: <img src="img/${player}.png" alt="scissors">, Computer chose: <img src="img/${computer}.png" alt="scissors">, <img src="img/${computer}.png" alt="scissors"> beats <img src="img/${player}.png" alt="scissors">, computer WINS!!
+    </h3>`
+  }  
   
-game()
-*/
+}
+
+
+  
